180e573df6b4ccf191732a619833e91435345b51
Joseph Welling <joseph.welling@hotmail.com>
2020-10-16 23:53:36 +0200

    📈 analytics beginning
    This reverts commit befc43f1fd9e6af36477d725a036c113a55193e4.

--- monitor.py
+++ monitor.py
@@ -3,6 +3,7 @@ import datetime
 s = speedtest.Speedtest()
 
 def record():
+  #create csv file with headers time and speed
   with open(f'{datetime.date.today()}_speed.csv', mode='w') as speedcsv:
     fieldnames = ['time', 'speed']
     csv_writer = csv.DictWriter(speedcsv, fieldnames=fieldnames)
@@ -14,6 +15,8 @@ def record():
       speed = round((round(s.download()) / 1048576), 2)
       print(speed, "Mb/s")
       csv_writer.writerow({'time': time, 'speed': speed})
+      #if day has changed, i.e past midnight, new csv file begins
+      #? maybe update this with generating graph then re-write?
       if datetime.date.today() != start_day:
         record()
 
