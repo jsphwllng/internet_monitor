aa5dce920dd45273ae814b7cfa88224178429e37
Joseph Welling <joseph.welling@hotmail.com>
2020-10-19 13:08:06 +0200

    🧑‍🎨📈 graph now rendering correctly

--- README.md
+++ README.md
@@ -3,9 +3,16 @@
 ## to do
 * <strike>monitor internet speed and write to a csv fille</strike>
 * write some analytics to return highest, average and lowest speeds
-<strike>* write a way to turn this into a graph</strike>
-<strike>* automate an upload of these files daily to a cloud storage system</strike>
+* <strike> write a way to turn this into a graph</strike>
+* <strike> automate an upload of these files daily to a cloud storage system</strike>
 
+
+## key learning points
+1. use of matplotlib
+2. environments and environment variables
+3. pydrive and automated file uploads
+4. benefits of a simple CSV over a database
+5. my internet is pretty inconsistent
 
 ## installation
 ```bash
--- monitor.py
+++ monitor.py
@@ -11,14 +11,13 @@ def record():
     csv_writer = csv.DictWriter(speedcsv, fieldnames=['time', 'speed'])
     csv_writer.writeheader()
     start_day = datetime.date.today()
-    count = 0
     while True:
       #writes into the CSV file until the date changes
       time = datetime.datetime.now().strftime("%H:%M")
       speed = round((round(s.download()) / 1048576), 2)
       csv_writer.writerow({'time': time, 'speed': speed})
       print({'time': time, 'speed': speed}, "Mb/s")
-      if datetime.date.today() == start_day:
+      if datetime.date.today() != start_day:
       #when day changes stop writing, generate a graph, upload that graph to google drive and start again
         speedcsv.close()
         make_graph(start_day)
