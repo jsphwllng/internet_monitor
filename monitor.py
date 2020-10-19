f821b341355d556845db4fd52ad8938959122e5c
Joseph Welling <joseph.welling@hotmail.com>
2020-10-19 10:20:50 +0200

    :woman_artist: graph rendering added

--- monitor.py
+++ monitor.py
@@ -11,11 +11,15 @@ def record():
     csv_writer.writeheader()
     start_day = datetime.date.today()
     while True:
-      time = datetime.datetime.now().strftime("%H:%M")
+      hours = datetime.datetime.now().strftime("%H")
+      minutes = datetime.datetime.now().strftime("%M")
       print(time)
       speed = round((round(s.download()) / 1048576), 2)
       print(speed, "Mb/s")
-      csv_writer.writerow({'time': time, 'speed': speed})
+      if minutes == "30" or minutes == "00":
+        csv_writer.writerow({'time': f"{hours}:{minutes}", 'speed': speed})
+      else:
+        csv_writer.writerow({'time': f"{hours}", 'speed': speed})
       #if day has changed, i.e past midnight, new csv file begins
       #? maybe update this with generating graph then re-write?
       if datetime.date.today() != start_day:
