e2a1f4dfeac97905975187a442bd3511b8b4be61
jsphwllng <joseph.welling@hotmail.com>
2020-10-22 00:03:50 +0200

    adding example

--- monitor.py
+++ monitor.py
@@ -15,20 +15,13 @@ def record():
     start_day = datetime.date.today()
     while True:
       #writes into the CSV file until the date changes
-<<<<<<< HEAD
-      time2 = datetime.datetime.now().strftime("%H:%M")
-      speed = round((round(s.download()) / 1048576), 2)
-      csv_writer.writerow({'time': time2, 'speed': speed})
-      print({'time': time2, 'speed': speed}, "Mb/s")
-      time.sleep(61)
-=======
       time = datetime.datetime.now().strftime("%H:%M")
       downspeed = round((round(s.download()) / 1048576), 2)
       upspeed = round((round(s.upload()) / 1048576), 2)
       csv_writer.writerow({'time': time, 'downspeed': downspeed, "upspeed": upspeed})
       print({'time': time, 'downspeed': downspeed, "upspeed": upspeed}, "Mb/s")
       t.sleep(60)
->>>>>>> b23e7e0a00d6cc55ae968a949d87f1572d5bcb8b
+
       if datetime.date.today() != start_day:
       #when day changes stop writing, generate a graph, upload that graph to google drive and start again
         speedcsv.close()
